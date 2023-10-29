d566bdfbeadc32726f2ff5f0f4e2037a8bb9d3f5
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2023-10-30 00:09:08 +0300

    add sorted_list_on_data function

--- src/processing.py
+++ src/processing.py
@@ -15,3 +15,18 @@ def filter_dict(list_data: List[dict], state: str = "EXECUTED") -> List[dict]:
         if data["state"] == state:
             filter_list.append(data)
     return filter_list
+
+
+def sorted_list_on_date(list_data: List[dict], order: bool = True) -> List[dict]:
+    """
+    Принимает список словарей в виде {'id': 594226727, 'state': 'EXECUTED', 'date': '2018-09-12T21:27:25.241689'}
+    возвращает список словарей отсортированных по значению ключа date
+    :param list_data: исходный список словарей
+    :param order: порядок по умолчанию True - сортировка по убыванию
+    :return: отфильтрованный список словарей по значению ключа state
+    """
+
+    if order:
+        return sorted(list_data, key=lambda x: x["date"], reverse=True)
+    else:
+        return sorted(list_data, key=lambda x: x["date"])

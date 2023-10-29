012045ac26c12212f89c9a3fe26961423633dadc
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2023-10-29 23:23:46 +0300

    add processing.py with filter_dict function

---
+++ src/processing.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def filter_dict(list_data: List[dict], state: str = "EXECUTED") -> List[dict]:
+    """
+    Принимает список словарей в виде {'id': 594226727, 'state': 'EXECUTED', 'date': '2018-09-12T21:27:25.241689'}
+    возвращает список словарей отфильтрованных по значению ключа state: EXECUTED или CANCELED
+    :param list_data: исходный список словарей
+    :param state: значение state для фильтрации, по умолчанию EXECUTED
+    :return: отфильтрованный список словарей по значению ключа state
+    """
+
+    filter_list: list[dict] = []
+    for data in list_data:
+        if data["state"] == state:
+            filter_list.append(data)
+    return filter_list

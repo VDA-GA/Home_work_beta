60a91eee1148b621043a59f93878940f1eb67a0a
Dmitry Vasilenko <vasilenkoda91@gmail.com>
2023-11-04 20:05:07 +0300

    fixed according tutor

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,27 +1,15 @@
-from src.masks import do_mask_account_number, do_mask_cart_number, make_dict_content_folder
+import pytest
+
+from src.masks import do_mask_account_number, do_mask_cart_number
 
 
-def test_do_mask_cart_number_for_str():
-    assert do_mask_cart_number('1234567891234567') == '1234 56** **** 4567'
+@pytest.mark.parametrize('cart_number, expected_result', [('1234567891234567', '1234 56** **** 4567'),
+                                                          (1234567891234567, '1234 56** **** 4567')])
+def test_do_mask_cart_number(cart_number, expected_result):
+    assert do_mask_cart_number(cart_number) == expected_result
 
 
-def test_do_mask_cart_number_for_int():
-    assert do_mask_cart_number(1234567891234567) == '1234 56** **** 4567'
-
-
-def test_do_mask_account_number_str():
-    assert do_mask_account_number('01234567890123456789') == '**6789'
-
-
-def test_do_mask_account_number_int():
-    assert do_mask_account_number(12345678901234567890) == '**7890'
-
-
-def test_make_dict_content_folder():
-    assert make_dict_content_folder(r'C:\Users\user\YandexDisk\SkyPro\lesson_code'
-                                    r'\pythonProject\tests\test_folder') == {'files': 1, 'folders': 1}
-
-
-def test_make_dict_content_folder_req():
-    assert make_dict_content_folder(r'C:\Users\user\YandexDisk\SkyPro\lesson_code'
-                                    r'\pythonProject\tests\test_folder', True) == {'files': 2, 'folders': 1}
+@pytest.mark.parametrize('account_number, expected_result', [('01234567890123456789', '**6789'),
+                                                             (12345678901234567890, '**7890')])
+def test_do_mask_account_number(account_number, expected_result):
+    assert do_mask_account_number(account_number) == expected_result
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -3,12 +3,11 @@ import pytest
 from src.widget import do_masks_number_card_or_account, get_data_from_time
 
 
-def test_do_masks_number_card_or_account_for_card():
-    assert do_masks_number_card_or_account('Visa Platinum 1234567890123456') == 'Visa Platinum 1234 56** **** 3456'
-
-
-def test_do_masks_number_card_or_account_for_account():
-    assert do_masks_number_card_or_account('Счет 12345678901234567890') == 'Счет **7890'
+@pytest.mark.parametrize('number, expected_result',
+                         [('Visa Platinum 1234567890123456', 'Visa Platinum 1234 56** **** 3456'),
+                          ('Счет 12345678901234567890', 'Счет **7890')])
+def test_do_masks_number_card_or_account_for_card(number, expected_result):
+    assert do_masks_number_card_or_account(number) == expected_result
 
 
 @pytest.mark.parametrize("data, expected_result", [('2018-07-11T02:26:18.671407', '11.07.2018'),
